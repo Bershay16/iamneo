cc0e3a6116d3d79bb4f0d743e6c8926dbf47c1b1
Bershay16 <2023it0079@svce.ac.in>
2026-08-19 21:30:39 +0530

    Near end to day 1

--- Day 1/Sliding Window/LongestConsecutiveSubstring.java
+++ Day 1/Sliding Window/LongestConsecutiveSubstring.java
@@ -1,3 +1,22 @@
 public class LongestConsecutiveSubstring {
-    
+    public static void main(String[] args) {
+        String s = "aaabbccccdde";
+
+        int max = 1, count = 1, end = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count > max) {
+                max = count;
+                end = i;
+            }
+        }
+
+        System.out.println(s.substring(end - max + 1, end + 1));
+    }
 }
--- Day 1/Sliding Window/PairAddition.java
+++ Day 1/Sliding Window/PairAddition.java
@@ -1,6 +1,18 @@
 public class PairAddition {
-    //Sliding Window Concept
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10};
+        int k = 2;
+        int sum = 0;
 
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println(sum);
+
+        for (int i = k; i < arr.length; i++) {
+            sum += arr[i] - arr[i - k];
+            System.out.println(sum);
+        }
     }
 }
